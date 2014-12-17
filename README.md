2436d470058635e2c307b2723276131eae9ca7a9
Conner McNamara <connermcnamara@gmail.com>
2014-12-17 11:18:01 -0800

    [FEATURE] Ability to get a ripple quote using the lib

---
+++ README.md
@@ -0,0 +1,4 @@
+bridge-quote-service-client
+====================
+[![Build Status](https://travis-ci.org/cdmcnamara/bridge-quote-service-client.svg?branch=master)](https://travis-ci.org/cdmcnamara/bridge-quote-service-client)
+[![Coverage Status](https://coveralls.io/repos/cdmcnamara/bridge-quote-service-client/badge.png)](https://coveralls.io/r/cdmcnamara/bridge-quote-service-client)
---
+++ test/lib/services/ripple_quote_service.js
@@ -0,0 +1,63 @@
+var util               = require('util');
+var chai               = require('chai');
+var chaiAsPromised     = require('chai-as-promised');
+var sinon              = require('sinon');
+var RippleRestClient   = require('ripple-rest-client');
+var winston            = require('winston');
+var RippleQuoteService = require(__dirname + '/../../../lib/services/ripple_quote_service.js');
+var fixture            = require(__dirname + '/../../fixtures/ripple_quote_requests.js');
+
+describe('ripple_quote', function() {
+
+  chai.use(chaiAsPromised);
+  var rippleQuoteService = new RippleQuoteService({
+    logger: winston,
+    rippleRestUrl: 'https://api.ripple.com/'
+  });
+
+  it('.validate() should successfully validate a ripple quote request', function(done) {
+    rippleQuoteService.validate(fixture.valid)
+      .then(done)
+      .catch(done);
+  });
+
+  it('.validate() should fail to validate a ripple quote (amount)', function() {
+    return chai.assert.isRejected(rippleQuoteService.validate(fixture.invalid.destination_amount), new Error('Destination amount is not valid number'));
+  });
+
+  it('.validate() should fail to validate a ripple quote (currency)', function() {
+    return chai.assert.isRejected(rippleQuoteService.validate(fixture.invalid.destination_currency), 'Currency is not valid');
+  });
+
+  it('.validate() should fail to validate a ripple quote (address)', function() {
+    return chai.assert.isRejected(rippleQuoteService.validate(fixture.invalid.destination_address), 'Address is not a valid ripple address');
+  });
+
+  it('.validate() should fail to validate a ripple quote (issuer)', function() {
+    return chai.assert.isRejected(rippleQuoteService.validate(fixture.invalid.source_address), 'Issuer is not a valid ripple address');
+  });
+
+  it('.build() calls ripple-rest with the provided args', function(done) {
+    var stub = sinon.stub(RippleRestClient.prototype, 'buildPayment')
+      .yields(null, fixture.ripple_rest_response.valid);
+    rippleQuoteService.build(fixture.valid)
+      .then(function() {
+        chai.assert.ok(stub.withArgs({
+          amount: fixture.valid.destination.amount,
+          currency: fixture.valid.destination.currency,
+          recipient: fixture.valid.destination.address,
+          source_currencies: fixture.valid.source.currencies
+        }).called);
+        stub.restore();
+        done();
+      })
+      .catch(function(error) {
+        stub.restore();
+        if (util.isError(error)) {
+          done(error);
+        } else {
+          done(new Error(JSON.stringify(error)));
+        }
+      });
+  });
+});
